cfb6e39736444eb25138abcc7981827b3e20ddad
Slava Rozhnev <slava.rozhnev@pandats.com>
2018-08-01 12:46:28 +0300

    migration table extended by migration name

--- query.js
+++ query.js
@@ -23,16 +23,16 @@ function execute_query(conn, path, final_file_paths, type, cb) {
     var current_file_path = path + "/" + file_name;
 
     var queries = require(current_file_path);
-    var timestamp_val = file_name.split("_", 1)[0];
+
     if (typeof(queries[type]) == 'string') {
       run_query(conn, queries[type], function (res) {
-        updateRecords(conn, type, table, timestamp_val, function () {
+        updateRecords(conn, type, table, file_name, function () {
           execute_query(conn, path, final_file_paths, type, cb);
         });
       });
     } else if (typeof(queries[type]) == 'function') {
       queries[type](conn, function() {
-        updateRecords(conn, type, table, timestamp_val, function () {
+        updateRecords(conn, type, table, file_name, function () {
           execute_query(conn, path, final_file_paths, type, cb);
         });
       });
@@ -43,10 +43,13 @@ function execute_query(conn, path, final_file_paths, type, cb) {
   }
 }
 
-function updateRecords(conn, type, table, timestamp_val, cb) {
+function updateRecords(conn, type, table, file_name, cb) {
   var query = '';
+  var timestamp_val = file_name.split("_", 1)[0];
+  var migration = file_name.split("_", 1)[1];
+
   if (type == 'up') {
-    query = "INSERT INTO " + table + " (`timestamp`) VALUES ('" + timestamp_val + "')";
+    query = "INSERT INTO " + table + " (`timestamp`) VALUES ('" + timestamp_val + "','" + migration + "')";
   } else if (type == 'down') {
     query = "DELETE FROM " + table + " WHERE `timestamp` = '" + timestamp_val + "'"
   }
