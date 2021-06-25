ed3943410237a9f0eb9e3f230dded62c8ee0faa0
Marek Sokol <12409688+mareksokol@users.noreply.github.com>
2021-06-25 08:15:00 +0200

    [RV2-54] Reworked mysql migrations (#1)
    * [RV2-54] Reworked mysql-migrations
    
    * Added github actions
    
    * Updated repo url

--- test/mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-
-module.exports = mysql.createPool({
-  connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'test_mig'
-});
